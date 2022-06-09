5573ee32c7e803640bb045b9c30285fd88f2e901
Hamiduh <hamiduhkisakye21@outlook.com>
2022-06-09 06:33:04 +0300

    first commit

---
+++ pages/success.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import { BsBagCheckFill } from "react-icons/bs";
+
+import { useStateContext } from "../context/StateContext";
+import { runFireworks } from "../lib/utils";
+
+const success = () => {
+  const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
+  useEffect(() => {
+    localStorage.clear();
+    setCartItems([]);
+    setTotalPrice(0);
+    setTotalQuantities(0);
+    runFireworks();
+  }, []);
+
+  return (
+    <div className="success-wrapper">
+      <div className="success">
+        <p className="icon">
+          <BsBagCheckFill />
+        </p>
+        <h2>Thank You For Your Order !!!</h2>
+        <p className="email-msg">Check Your Email Inbox For Your Receipt.</p>
+        <p className="description">
+          If You Have Any Questions, Please Email:
+          <a href="mailto:kisakyehamiduh@gmail.com" className="email">
+            kisakyehamiduh@gmail.com
+          </a>
+        </p>
+        <Link href="/">
+          <button type="button" width="300px" className="btn">
+            Continue Shopping
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default success;
